594a761f8060443693dcf2bfad210c0767b64b74
SmatovE <black63squad@gmail.com>
2024-07-03 18:19:52 +0500

    update

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const cors = require('cors');
 const token = "6877036574:AAH09MxxWf4Lzesz_6EXnhUdsFM1dDim9XE";
 const webAppUrl = "https://tg-web-app-bot.netlify.app"
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
-
+const bot = new TelegramBot(token, {polling: true});
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -14,65 +14,58 @@ bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
-    if (text === "/start") {
-        await bot.sendMessage(chatId, 'Добро пожаловать!', {
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
             reply_markup: {
                 keyboard: [
-                    [{ text: 'Открыть форму', web_app: {url: webAppUrl + '/form'} }]
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                ]
+            }
+        })
+
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
                 ]
             }
         })
     }
 
-    // await bot.sendMessage(chatId, 'Inline button', {
-    //     reply_markup: {
-    //         inline_keyboard: [
-    //             [{ text: 'Сделать заказ' }]
-    //         ]
-    //     }
-    // })
-
-    if (msg?.web_app_data?.data) {
+    if(msg?.web_app_data?.data) {
         try {
             const data = JSON.parse(msg?.web_app_data?.data)
-            console.log(data);
-            await bot.sendMessage(chatId, 'Спасибо за обратную связь');
-            await bot.sendMessage(chatId, 'Ваша страна: ' + data.country);
-        } catch (error) {
-            console.log(error);
+            console.log(data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
+
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
+        } catch (e) {
+            console.log(e);
         }
     }
-
-    //   bot.sendMessage(chatId, 'Received your message');
 });
 
-app.post('web-data', async (req, res) => {
-    const {queryId, products, totalPrice} = req;
-    await bot.answerWebAppQuery(queryId, {
-        type: 'article',
-        id: queryId,
-        title: 'sending data',
-        input_message_content: {message_text: JSON.stringify(products)}
-    })
+app.post('/web-data', async (req, res) => {
+    const {queryId, products = [], totalPrice} = req.body;
     try {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
             id: queryId,
             title: 'Успешная покупка',
-            input_message_content: {message_text: 'Поздравляю с покупкой, вы приобрели товар на сумму ' + totalPrice}
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
         })
-
         return res.status(200).json({});
-    } catch (error) {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Ошибка покупки',
-            input_message_content: {message_text: 'Не удалось приобрести товар'}
-        })
-        return res.status(500).json({});
+    } catch (e) {
+        return res.status(500).json({})
     }
 })
 
 const PORT = 8000;
-app.listen(PORT, () => console.log('Server started on PORT: ' + PORT));
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
